ca13a4aa32b281b86d1515b65b6d06f1ba0b9c7a
Gabriel Santos <gab.tec97@gmail.com>
2022-08-21 20:29:27 -0300

    Refatora o código usando um objeto JS e fazendo melhor uso dos dados da API dentro da função showRepos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,144 +1,259 @@
-tecos_nomes = [
-  "Ana Carolina",
-  "Ana Clara",
-  "Beatriz Ramalho",
-  "Breno Martins",
-  "Bruna Andrade",
-  "Cainã Lucas",
-  "Caio Peternela",
-  "Clara Santos",
-  "Daniel Alves",
-  "Daniel Dantas",
-  "David Satoshi",
-  "Edinaildo Costa",
-  "Eliel Clementino",
-  "Evandro Pereira",
-  "Fabricio Assunção",
-  "Felipe Soares",
-  "Fernanda Liz",
-  "Gabriel Santos",
-  "Gabriela Victoria",
-  "Gislane Nunes",
-  "Glória Felix",
-  "Ingrid Caroline",
-  "João Vitor",
-  "Laura Nascimento",
-  "Lorrana Camargo",
-  "Lucas Mariano",
-  "Lucca Nunes",
-  "Marcela Lima",
-  "Marcelo Henrique",
-  "Marcos Ornellas",
-  "Marcos Viana",
-  "Marília Moraes",
-  "Rafael Leone",
-  "Roberto de Moraes Zarzur",
-  "Ronald Perdigão",
-  "Sara Brangioni Santa Rita",
-  "Tábata de Faria",
-  "Victor Magalhães Barreto",
-  "Victor Prata",
-];
-tecos_user = [
-  "anacmas",
-  "Dfinal_paginapessoal",
-  "Beatrizresteves",
-  "oBrenoMS",
-  "andradebru",
-  "paginaoficial",
-  "caiopeternela",
-  "ClaraSantosmf",
-  "inczDan",
-  "DaniDanis",
-  "davidsatag",
-  "edinaildo",
-  "ElielClementino",
-  "evandropcs",
-  "fbc00",
-  "felipesoaresfl",
-  "fernandalizs",
-  "gabriels999",
-  "GabyVictoria0122",
-  "gf-nunes",
-  "globrito",
-  "carolineingrid",
-  "joaoprocopio",
-  "launasci",
-  "lorrananeves",
-  "lukemariano",
-  "luccaribeiro",
-  "marcelavie",
-  "mhenrique94",
-  "129-ornellas",
-  "MarcosGrandesso",
-  "marilia-borgo",
-  "rafaelleone",
-  "Rdemora2",
-  "ronald-ps",
-  "sarabrangioni",
-  "tabataf",
-  "VictorMagalhaesBarreto",
-  "ovictorprata",
-];
-tecos_repos = [
-  "Desafio-final",
-  "AnaClaraCoelho",
-  "Desafio_final_pgpessoal",
-  "d8.2",
-  "dfinal-minha-pagina",
-  "cainlucas",
-  "dfinal-pagina-pessoal/pagina-pessoal/index.html",
-  "BuserTech-D8-Personal-page",
-  "D8-PgPessoal",
-  "D--Final",
-  "Pagina_pessoal",
-  "Desafio-Final",
-  "D9_minha_pagina_pessoal",
-  "Pagina-Pessoal",
-  "p-gina-pessoal-desafioFinal",
-  "busertech-D9-personal-page",
-  "Desafio-Final",
-  "DF-PaginaPessoal",
-  "Minha-Pagina-Pessoal",
-  "D9.P-ginaPessoal",
-  "d.9_pagina_pessoal_final_",
-  "D-Final-Pagina-Pessoal",
-  "dfinal-pag-pessoal",
-  "D.FinalPagPessoal",
-  "dfinal",
-  "D9-Pagina-Pessoal",
-  "d9-final",
-  "Pagina-pessoal",
-  "DFinal-Pag_Pessoal",
-  "P-gina-Pessoal---Marcos-Ornellas",
-  "D_Final_Pag_pessoal_marcos",
-  "Portfolio_2022",
-  "dfinal/index.html",
-  "D.Final---Pagina_Pessoal",
-  "Page-ronald",
-  "P-gina-Pessoal",
-  "Tabata-pagina-pessoal",
-  "D-Final",
-  "https://bit.ly/3lUllvf",
+let lista = [
+  {
+    nome: "Ana Carolina",
+    repo: "Desafio-final",
+    deploy_gh: "https://anacmas.github.io/Desafio-final/",
+    username: "anacmas",
+  },
+  {
+    nome: "Ana Clara",
+    repo: "Dfinal_paginapessoal",
+    deploy_gh: "https://anaclaracoelho.github.io/Dfinal_paginapessoal/",
+    username: "AnaClaraCoelho",
+  },
+  {
+    nome: "Beatriz Ramalho",
+    repo: "Desafio_final_pgpessoal",
+    deploy_gh: "https://beatrizresteves.github.io/Desafio_final_pgpessoal/",
+    username: "Beatrizresteves",
+  },
+  {
+    nome: "Breno Martins",
+    repo: "d8.2",
+    deploy_gh: "https://obrenoms.github.io/d8.2/",
+    username: "oBrenoMS",
+  },
+  {
+    nome: "Bruna Andrade",
+    repo: "dfinal-minha-pagina",
+    deploy_gh: "https://andradebru.github.io/dfinal-minha-pagina/",
+    username: "andradebru",
+  },
+  {
+    nome: "Cainã Lucas",
+    repo: "paginaoficial",
+    deploy_gh: "https://cainlucas.github.io/paginaoficial/",
+    username: "cainlucas",
+  },
+  {
+    nome: "Caio Peternela",
+    repo: "dfinal-pagina-pessoal/pagina-pessoal/index.html",
+    deploy_gh:
+      "https://caiopeternela.github.io/dfinal-pagina-pessoal/pagina-pessoal/index.html",
+    username: "caiopeternela",
+  },
+  {
+    nome: "Clara Santos",
+    repo: "BuserTech-D8-Personal-page",
+    deploy_gh: "https://clarasantosmf.github.io/BuserTech-D8-Personal-page/",
+    username: "ClaraSantosmf",
+  },
+  {
+    nome: "Daniel Alves",
+    repo: "D8-PgPessoal",
+    deploy_gh: "https://inczdan.github.io/D8-PgPessoal/",
+    username: "inczDan",
+  },
+  {
+    nome: "Daniel Dantas",
+    repo: "D--Final",
+    deploy_gh: "https://danidanis.github.io/D--Final/",
+    username: "DaniDanis",
+  },
+  {
+    nome: "David Satoshi",
+    repo: "Pagina_pessoal",
+    deploy_gh: "https://davidsatag.github.io/Pagina_pessoal/",
+    username: "davidsatag",
+  },
+  {
+    nome: "Edinaildo Costa",
+    repo: "Desafio-Final",
+    deploy_gh: "https://edinaildo.github.io/Desafio-Final/",
+    username: "edinaildo",
+  },
+  {
+    nome: "Eliel Clementino",
+    repo: "D9_minha_pagina_pessoal",
+    deploy_gh: "https://elielclementino.github.io/D9_minha_pagina_pessoal/",
+    username: "ElielClementino",
+  },
+  {
+    nome: "Evandro Pereira",
+    repo: "Pagina-Pessoal",
+    deploy_gh: "https://evandropcs.github.io/Pagina-Pessoal/",
+    username: "evandropcs",
+  },
+  {
+    nome: "Fabricio Assunção",
+    repo: "p-gina-pessoal-desafioFinal",
+    deploy_gh: "https://fbc00.github.io/p-gina-pessoal-desafioFinal/",
+    username: "fbc00",
+  },
+  {
+    nome: "Felipe Soares",
+    repo: "busertech-D9-personal-page",
+    deploy_gh: "https://felipesoaresfl.github.io/busertech-D9-personal-page/",
+    username: "felipesoaresfl",
+  },
+  {
+    nome: "Fernanda Liz",
+    repo: "Desafio-Final",
+    deploy_gh: "https://fernandalizs.github.io/Desafio-Final/",
+    username: "fernandalizs",
+  },
+  {
+    nome: "Gabriel Santos",
+    repo: "DF-PaginaPessoal",
+    deploy_gh: "https://gabriels999.github.io/DF-PaginaPessoal/",
+    username: "Gabriels999",
+  },
+  {
+    nome: "Gabriela Victoria",
+    repo: "Minha-Pagina-Pessoal",
+    deploy_gh: "https://gabyvictoria0122.github.io/Minha-Pagina-Pessoal/",
+    username: "GabyVictoria0122",
+  },
+  {
+    nome: "Gislane Nunes",
+    repo: "D9.P-ginaPessoal",
+    deploy_gh: "https://gf-nunes.github.io/D9.P-ginaPessoal/",
+    username: "gf-nunes",
+  },
+  {
+    nome: "Glória Felix",
+    repo: "d.9_pagina_pessoal_final_",
+    deploy_gh: "https://globrito.github.io/d.9_pagina_pessoal_final_/",
+    username: "globrito",
+  },
+  {
+    nome: "Ingrid Caroline",
+    repo: "D-Final-Pagina-Pessoal",
+    deploy_gh: "https://carolineingrid.github.io/D-Final-Pagina-Pessoal/",
+    username: "carolineingrid",
+  },
+  {
+    nome: "João Vitor",
+    repo: "dfinal-pag-pessoal",
+    deploy_gh: "https://joaoprocopio.github.io/dfinal-pag-pessoal/",
+    username: "joaoprocopio",
+  },
+  {
+    nome: "Laura Nascimento",
+    repo: "D.FinalPagPessoal",
+    deploy_gh: "https://launasci.github.io/D.FinalPagPessoal/",
+    username: "launasci",
+  },
+  {
+    nome: "Lorrana Camargo",
+    repo: "dfinal",
+    deploy_gh: "https://lorrananeves.github.io/dfinal/",
+    username: "lorrananeves",
+  },
+  {
+    nome: "Lucas Mariano",
+    repo: "D9-Pagina-Pessoal",
+    deploy_gh: "https://lukemariano.github.io/D9-Pagina-Pessoal/",
+    username: "lukemariano",
+  },
+  {
+    nome: "Lucca Nunes",
+    repo: "d9-final",
+    deploy_gh: "https://luccaribeiro.github.io/d9-final/",
+    username: "luccaribeiro",
+  },
+  {
+    nome: "Marcela Lima",
+    repo: "Pagina-pessoal",
+    deploy_gh: "https://marcelavie.github.io/Pagina-pessoal/",
+    username: "marcelavie",
+  },
+  {
+    nome: "Marcelo Henrique",
+    repo: "DFinal-Pag_Pessoal",
+    deploy_gh: "https://mhenrique94.github.io/DFinal-Pag_Pessoal/",
+    username: "mhenrique94",
+  },
+  {
+    nome: "Marcos Ornellas",
+    repo: "P-gina-Pessoal---Marcos-Ornellas",
+    deploy_gh:
+      "https://129-ornellas.github.io/P-gina-Pessoal---Marcos-Ornellas/",
+    username: "129-ornellas",
+  },
+  {
+    nome: "marcos viana",
+    repo: "D_Final_Pag_pessoal_marcos",
+    deploy_gh: "https://marcosgrandesso.github.io/D_Final_Pag_pessoal_marcos/",
+    username: "MarcosGrandesso",
+  },
+  {
+    nome: "Marília Moraes",
+    repo: "Portfolio_2022",
+    deploy_gh: "https://marilia-borgo.github.io/Portfolio_2022/",
+    username: "marilia-borgo",
+  },
+  {
+    nome: "Rafael Leone",
+    repo: "dfinal/index.html",
+    deploy_gh: "https://rafaelleone.github.io/dfinal/index.html",
+    username: "rafaelleone",
+  },
+  {
+    nome: "Roberto de Moraes Zarzur",
+    repo: "D.Final---Pagina_Pessoal",
+    deploy_gh: "https://rdemora2.github.io/D.Final---Pagina_Pessoal/",
+    username: "Rdemora2",
+  },
+  {
+    nome: "Ronald Perdigão",
+    repo: "Page-ronald",
+    deploy_gh: "https://ronald-ps.github.io/Page-ronald/",
+    username: "ronald-ps",
+  },
+  {
+    nome: "Sara Brangioni Santa Rita",
+    repo: "P-gina-Pessoal",
+    deploy_gh: "https://sarabrangioni.github.io/P-gina-Pessoal/",
+    username: "sarabrangioni",
+  },
+  {
+    nome: "Tábata de Faria",
+    repo: "Tabata-pagina-pessoal",
+    deploy_gh: "https://tabataf.github.io/Tabata-pagina-pessoal/",
+    username: "tabataf",
+  },
+  {
+    nome: "Victor Magalhães Barreto",
+    repo: "D-Final",
+    deploy_gh: "https://victormagalhaesbarreto.github.io/D-Final/",
+    username: "VictorMagalhaesBarreto",
+  },
+  {
+    nome: "Victor Prata",
+    repo: "d9-pagina-pessoal",
+    deploy_gh: "https://bit.ly/3lUllvf",
+    username: "ovictorprata",
+  },
 ];
 
-function showRepos(data) {
+function showRepos(data, indice) {
   const features = document.querySelector(".features");
-  let link = data.url;
+  let link = `https://${data.owner.login}.github.io/${data.name}/`;
   let stars = data.stargazers_count;
   const article = document.createElement("article");
 
   const h3 = document.createElement("h3");
   article.appendChild(h3);
   h3.classList.add("major");
-  h3.textContent = tecos_nomes[i];
+  h3.textContent = lista[indice].nome;
 
   const a = document.createElement("a");
   a.classList.add("special");
   article.appendChild(a);
   a.setAttribute("href", link);
   a.setAttribute("target", "_blank");
-  a.textContent = tecos_user[i];
+  a.textContent = data.owner.login;
 
   const span = document.createElement("span");
   article.appendChild(span);
@@ -146,8 +261,8 @@ function showRepos(data) {
   features.appendChild(article);
 }
 
-for (i = 0; i < tecos_nomes.length; i++) {
-  fetch(`https://api.github.com/repos/${tecos_user[i]}/${tecos_repos[i]}`)
+for (i = 0; i < lista.length; i++) {
+  fetch(`https://api.github.com/repos/${lista[i].username}/${lista[i].repo}`)
     .then((response) => response.json())
-    .then((data) => showRepos(data));
+    .then((data) => showRepos(data, i));
 }
